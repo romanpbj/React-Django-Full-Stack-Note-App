3b872cc0800d4b462ca39a3e4ce800aeb0e4c7b5
unknown <romannn713@gmail.com>
2024-09-26 00:17:08 -0700

    add backend url

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,8 +1,10 @@
 import axios from "axios";
 import { ACCESS_TOKEN } from "./constants";
 
+const apiURL = /choreo-apis/django-react-note-app/backend/v1
+
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL
+  baseURL: import.meta.env.VITE_API_URL ? import.meta.env.VITE_API_URL : apiURL,
 });
 
 api.interceptors.request.use(
